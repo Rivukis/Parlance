703a240fead7383cf90078b31e033840b75c99eb
Brian Radebaugh <rivukis@gmail.com>
2017-02-08 21:58:56 -0800

    added Test target

---
+++ ParlanceTests/ParlanceCoordinatableTests.swift
@@ -0,0 +1,39 @@
+//
+//  ParlanceCoordinatableSpec.swift
+//  Parlance
+//
+//  Created by Brian Radebaugh on 2/8/17.
+//  Copyright © 2017 Brian Radebaugh. All rights reserved.
+//
+
+import XCTest
+
+class ParlanceCoordinatableTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testCurrentLanguage() {
+        
+    }
+    
+}
